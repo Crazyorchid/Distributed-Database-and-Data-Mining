ae426eff0040beefd2266d64399c5b8510ef9f17
a1778478 <a1778478@f124924c-131e-4e9b-aee0-b9ab63c2b873>
2020-09-20 09:06:24 +0000

    try

---
+++ assignment1/AAGenerator.cpp
@@ -0,0 +1,218 @@
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <tgmath.h>
+
+using namespace std;
+
+class toAA{
+    public:
+    int queryS[50][50];
+    int wqueryS = 0; //width of the query;
+    int hqueryS = 0; // height of the query;
+
+    string bufftmp[2];
+    int queryA[50][50];
+    int wqueryA = 0;
+    int hqueryA = 0;
+
+    int aa[50][50];
+
+
+void readattAndQuery ()
+{
+
+    //read the aa test file and put into a vector;
+    ifstream aa("aa_1.txt");
+    string first_line;
+    vector<string> read;
+    try{
+        if(aa){
+            while(getline(aa, first_line)){
+                read.push_back(first_line);
+            }
+        }
+    } catch (const char* msg) {
+        cout << msg << endl;
+    }
+    // cout the number stored in the txt to vector
+
+    ifstream qu("query_1.txt");
+    string second_line;
+    vector<string> read2;
+    try{
+        if(qu){
+            while(getline(qu, second_line)){
+                read2.push_back(second_line);
+            }
+        }
+    } catch (const char* msg) {
+        cout << msg << endl;
+    }
+    hqueryA = read2.size();
+    wqueryA = read2.size()-1;
+
+    //the matrix of aa nad query and put into query2[][];
+
+    for (int i = 0; i < read2.size(); i++) {
+        for (int j = 0; j < read.size(); j++) {
+            string buff;
+            stringstream ss(read[j]);
+            for (int k = 0; k < 2; k++) {
+                if (ss >> buff){
+                    bufftmp[k] = buff;
+                }
+            }
+            bool found = findATT(read2[i],bufftmp[1]);
+            if(found){
+                queryA[i][j-1] = 1;
+            }else {
+                queryA[i][j - 1] = 0;
+            }
+
+        }
+        }
+
+    }
+    bool findATT(string a, string b) {
+        int alength = a.length();
+        int blength = b.length();
+        bool is = false;
+        for (int i = 8; i < alength; i++) {
+            if (a[i] == b[0] && alength - i >= blength) {
+                if (a.substr(i, blength) == b) {
+                    if (a[i - 1] - '0' < 17 || a[i - 1] - '0' > 42) {
+                        if (alength - i == blength) {
+                            is = true;
+                        } else {
+                            if (a[i + blength] - '0' < 17 || a[i+blength] - '0' > 42) {
+                                is = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return is;
+    }
+
+
+
+void readacc(){
+    ifstream  in ("acc_1.txt");
+    string line;
+    vector<string> t;
+    try{
+        if(in){
+            while(getline(in, line)){
+                t.push_back(line);
+            }
+        }
+    } catch (const char* msg) {
+        cout << msg << endl;
+    }
+    hqueryS = t.size() - 1;
+
+    for(int i=1; i < t.size();i++){
+        string temp = t[i];
+        string buf;
+        stringstream ss(temp);
+        int num = 0;
+        while(ss >> buf){
+            if(num>0){
+                stringstream convert(buf);
+                convert>> queryS[i-1][num-1];
+            }
+            num++;
+            if(num-1>wqueryS){
+                wqueryS=num-1;
+            }
+        }
+
+    }
+}
+
+void makeAA(){
+    for (int i = 0; i < wqueryA; i++) {
+        for (int j = 0; j < wqueryA; j++) {
+            if(i<=j){
+                doaff(i,j);
+            }else{
+                aa[i][j]=aa[j][i];
+            }
+
+        }
+
+    }
+}
+
+void doaff(int i, int j){
+
+    float a = 0;
+    float b0 = 0;
+    float b1 = 0;
+    for(int k=0; k<hqueryA;k++){
+        float Aik;
+        float Ajk;
+        float QkSj = 0;
+        for (int s = 0; s < wqueryS; s++) {
+            QkSj+=(float)queryS[k][s];
+        }
+        Ajk = (float)queryA[k][i]*QkSj;
+        Ajk = (float)queryA[k][j]*QkSj;
+
+        a+=Aik*Ajk;
+        b0 += Aik;
+        b1 += Ajk;
+    }
+    float bfinal = pow(b0*b1, 0.5);
+    if(bfinal==0){
+        aa[i][j] = 0;
+
+    }else{
+        float result = a/bfinal;
+        int resultInt = (int)result;
+        if(result>resultInt){
+            resultInt++;
+        }
+        aa[i][j]=resultInt;
+    }
+}
+void printaa(){
+    for (int i = 0; i < wqueryA; i++) {
+        for (int j = 0; j < wqueryA; j++) {
+            if(j ==wqueryA-1 ){
+                cout<<aa[i][j]<<endl;
+            }else{
+                cout<<aa[i][j]<<" ";
+            }
+        }
+    }
+}
+void printqa(){
+    for (int i = 0; i < wqueryA; i++) {
+        for (int j = 0; j < wqueryA; j++) {
+            if(j ==wqueryA-1 )
+                cout<<queryA[i][j]<<endl;
+            else
+                cout<<queryA[i][j]<<" ";
+            }
+        }
+    }
+};
+
+
+int main()
+{
+    toAA myobj;
+    myobj.readattAndQuery();
+    myobj.readacc();
+
+    myobj.makeAA();
+    myobj.printaa();
+
+    return 0;
+
+
+}
---
+++ assignment1/CAGenerator.cpp
@@ -0,0 +1,174 @@
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <tgmath.h>
+
+using namespace std;
+
+class AAtoCA{
+public:
+    int aa[30][30];
+    int ca[30][30];
+    int insertcolumn[30];
+    int sequence[30];
+    int length;
+    int cont = 0;
+    int bond = 0;
+    ofstream outf;
+
+    void readaa(vector<string>x){
+        length = x.size();
+
+        for (int i = 0; i < length; i++) {
+            string temp = x[i];
+            string buf;
+            stringstream ss(temp);
+            for (int j = 0; j < length; j++) {
+                if(ss>>buf){
+                    stringstream convert(buf);
+                    convert >> aa[i][j];
+                }
+
+            }
+
+        }
+    }
+    void toca(){
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < 2; j++) {
+                ca[i][j]=aa[i][j];
+
+            }
+
+        }
+        for (int i = 0; i < 2; i++) {
+            sequence[i]=i;
+            }
+        for (int j = 0; j < length; j++) {
+            for (int i = 0; i < length; i++) {
+                insertcolumn[i] = aa[i][j];
+            }
+            int maxcont = 0;
+            int loc = 0;
+            for (int num = 0; num < j; num++) {
+                docont(num,j);
+                if(cont > maxcont){
+                    maxcont = cont;
+                    loc = num;
+
+            }
+
+        }
+            for (int x = j; x >=loc; x--) {
+                for (int i = 0; i < length; i++) {
+                    ca[i][x+i]=ca[i][x];
+                }
+                sequence[x+1]=sequence[x];
+            }
+            for (int i = 0; i < length; i++) {
+                ca[i][loc] = insertcolumn[i];
+            }
+            sequence[loc]=j;
+
+        }
+    }
+    void docont(int num, int j){
+        if(num==0){
+            dobond(0, true);
+            cont = bond;
+        }
+        if(num==j){
+            dobond(j-1,true);
+            cont = bond;
+        }
+        if(num!=0&&num!=j){
+            dobond(num-1,true);
+            int bond0 = bond;
+            dobond(num, true);
+            int bond1 = bond;
+            dobond(num,false);
+            int bond2 = bond;
+            cont = bond0+bond1-bond2;
+        }
+    }
+    void dobond(int a, bool b){
+        bond=0;
+        if(b==true){
+            for (int i = 0; i < length; i++) {
+                bond+=ca[i][a]*insertcolumn[i];
+            }
+        }else{
+            for (int i = 0; i < length; i++) {
+                bond+=ca[i][a-1]*ca[i][a];
+
+            }
+        }
+
+    }
+
+    void turnover(){
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                aa[j][i]=ca[i][j];
+            }
+        }
+    }
+    void printsequence(){
+        outf.open("ca_1.txt");
+        cout<<" ";
+        for (int i = 0; i < length; i++) {
+            cout << "A" <<sequence[i] +1<<" ";
+            cout << "A" <<sequence[i] +1<<" ";
+        }
+        outf<<endl;
+        cout<<endl;
+    }
+    void printca(){
+        for (int i = 0; i < length; i++) {
+            outf << "A" <<sequence[i] + 1 << " ";
+            cout << "A" <<sequence[i] + 1 << " ";
+            for (int j = 0; j < length; j++) {
+                outf<<ca[i][j];
+                cout<<ca[i][j];
+                if(j==length-1){
+                    outf<<endl;
+                    cout<<endl;
+                }else{
+                    outf<<" ";
+                    cout<<" ";
+                }
+
+            }
+
+        }
+        outf.close();
+    }
+};
+int main(int argc, char* argv[])
+{
+    ifstream in(argv[1]);
+    string line;
+    vector<string>aa;
+
+    try{
+        if(in){
+            while(getline(in,line)){
+                aa.push_back(line);
+            }
+        }
+    }catch(const char* msg){
+        cout <<msg <<endl;
+    }
+    AAtoCA myobj;
+    myobj.readaa(aa);
+    myobj.toca();
+
+    myobj.printsequence();
+    myobj.turnover();
+    myobj.toca();
+    myobj.printca();
+
+    return 0;
+
+}
